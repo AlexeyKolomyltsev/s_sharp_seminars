33dd276759cb5c782a6405b1526e5bc1f6f518c0
AlexeyWatt <allexdoma2@gmail.com>
2022-04-26 12:36:39 +0300

    add последнюю цифру

---
+++ 05_poslednya_cifra/Program.cs
@@ -0,0 +1,7 @@
+﻿///    Console.WriteLine("Put number xxx");
+///    string b = Console.ReadLine();
+///    int c = b.Length;
+///    Console.WriteLine(c);
+Console.WriteLine("Put number xxx");
+int a = int.Parse(Console.ReadLine()) % 10;
+Console.WriteLine(a);
