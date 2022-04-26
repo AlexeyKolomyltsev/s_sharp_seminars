68a7ebfdaa118ae06adc12e5f4780a0e8b798d9d
AlexeyWatt <allexdoma2@gmail.com>
2022-04-26 11:21:33 +0300

    add 02 проверка на квадрат числа

---
+++ 02_provaerka_kvadrata/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Put number a ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Put number b ");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{   Console.WriteLine("Number a more then number b");
+    if (a == b * b)
+    {
+        Console.WriteLine("Yes, number a=b^2");
+    }
+    else {
+        Console.WriteLine("No, number a!=b^2");
+    }
+}
+else
+{   Console.WriteLine("Number b more then number a");
+    if (b == a * a)
+    {
+        Console.WriteLine("Yes, number b=a^2");
+    }
+    else{
+        Console.WriteLine("No, number b!=a^2");
+    }
+}
