4cb00b2ab3b78ca79fa0ad5d38eb89357a24f648
AlexeyWatt <allexdoma2@gmail.com>
2022-04-26 11:39:28 +0300

    add 03 проверка на max из 3х цифр

---
+++ 03_max_number/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Put number a ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Put number b ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Put number c ");
+int c = int.Parse(Console.ReadLine());
+int max;
+if (a > b)
+{
+    max = a;
+    if (c > max) max = c;
+}
+else
+{
+    max = b;
+    if (c > max) max = c;
+}
+Console.Write("Max = ");
+Console.Write(max);
